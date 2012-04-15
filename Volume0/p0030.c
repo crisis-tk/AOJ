77954b66af676bc1939761a0814e6ea35e735ac8
Kouta TOUME <ahagonn2000@gmail.com>
2012-04-16 00:56:43 +0900

    add 0030 (Time Limit Exceeded)

---
+++ Volume0/p0030.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int count = 0;
+
+/*
+ *  深さ優先探索
+ *  n:項の数, s:合計, depth:深さ, index:ノードの値
+ */
+int dfs(int n, int s, int depth, int index)
+{
+    int sum = s - index;
+
+    /* 深さ(=depth+1) が n と等しい かつ
+     * 合計(=sum) が s と等しい とき count++ ;
+     */
+    if ( depth+1 == n && sum == 0 ) {
+        count++;
+        return 0;
+    }
+    
+    if ( depth+1 < n ) {
+        int i;
+        for (i = index+1; i <= sum; i++) {
+            dfs(n, sum, depth+1, i);
+        }
+    }
+    return 0;
+}
+
+int main(void)
+{
+    int n, s;
+
+    while(1) {
+        scanf("%d %d", &n, &s);
+
+        if ( n == 0 && s == 0) {
+            break;
+        }
+
+        int i;
+        for (i = 0; i <= s; i++) {
+            dfs(n, s, 0, i);
+        }
+        printf("%d\n", count);
+
+        count = 0;
+    }
+    
+    return 0;
+}
